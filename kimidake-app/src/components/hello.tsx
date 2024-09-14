a8bddde79193c266aaef9b4f86c75ad70eabb673
kikunagahonoka <kikunaga.honoka.kut@gmail.com>
2024-09-15 03:23:04 +0900

    最初の諸設定

---
+++ kimidake-app/src/components/hello.tsx
@@ -0,0 +1,11 @@
+import { View, Text, StyelSheet } from "react-native";
+
+const Hello = (): JSX.Element => {
+    return(
+        <View>
+            <Text>Hello</Text>
+        </View>
+    )
+}
+
+export default Hello
